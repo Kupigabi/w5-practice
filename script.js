e2a3b4ad2940ce2dc49a836feee3056a0e30253c
Kupigabi <gabor.kupeczki@gmail.com>
2023-10-26 10:45:42 +0200

    fetch component added

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ console.log('loaded')
 const rootElement = document.querySelector("#root")
 console.dir(rootElement)
 
+const fetchUrl = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
 const countryComponent = (country) => `
     <div class="country">
         <h2>country name: ${country.name.common}</h2>
@@ -11,8 +17,7 @@ const countryComponent = (country) => `
 `
 
 async function init() { // felkészítjük a js-t, hogy a függvényben lesznek aszinkron kódok
-    const response = await fetch("https://restcountries.com/v3.1/all") // megvárjuk a fetch válaszát (promise helyett)
-    const data = await response.json() // megvárjuk, hogy a response megjöjjön (promise helyett) -> itt lesz elérhető az adatunk
+    const data = await fetchUrl("https://restcountries.com/v3.1/all")
 
     data.forEach(country => rootElement.insertAdjacentHTML("beforeend", countryComponent(country)))
 }
